e7f8c74de32d2c46a8d50b4551e39aa1b42fa182
marovichn <nikolamarovic.11@gmail.com>
2023-08-25 16:02:03 +0200

    Navbar, Welcome

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {HiMenuAlt4} from "react-icons/hi"
+import {AiOutlineClose} from "react-icons/ai"
+
+const Navbar = () => {
+  return (
+    <div className='text-white'><HiMenuAlt4/></div>
+  )
+}
+
+export default Navbar
---
+++ client/src/components/Welcome.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <div className='text-white'>Welcome</div>
+  )
+}
+
+export default Welcome
