334e8b1b4ea0424e051d6f24965f30611edc2349
marovichn <nikolamarovic.11@gmail.com>
2023-08-27 16:10:16 +0200

    Smart contract init, dependencies

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -16,26 +16,30 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
     min="0.0000001"
     value={value}
     onChange={(e) => handleChange(e, name)}
-    className='my-2 w-full p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism'
+    className='my-2 w-full p-2 outline-none bg-transparent text-white border-none text-sm blue-glassmorphism'
   />
 );
 
 const Welcome = () => {
+const connectWallet =()=>{};
+const handleSubmit =()=>{};
+
+
   return (
     <div className='flex w-full justify-center items-center'>
       <div className='flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4'>
         <div className='flex flex-1 justify-start items-start flex-col mf:mr-10'>
-          <h1 className='text-3xl sm:text-5xl text-white text-gradient py-1 font-bold'>
+          <h1 className='text-3xl sm:text-5xl text-white  py-1 font-bold'>
             Send Crypto <br /> across the world
           </h1>
           <p className='text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base'>
             Explore the crypto world. Buy and sell cryptocurrencies easily on
-            Krypto.
+            Cryptio.
           </p>
           
             <button
               type='button'
-              onClick={()=>{}}
+              onClick={connectWallet}
               className='flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]'
             >
               <AiFillPlayCircle className='text-white mr-2' />
@@ -107,14 +111,14 @@ const Welcome = () => {
               handleChange={()=>{}}
             />
 
-            <div className='h-[1px] w-full bg-gray-400 my-2' />
+            <div className='h-[1px] w-full bg-blue-900 my-2' />
 
             {false ? (
               <Loader />
             ) : (
               <button
                 type='button'
-                onClick={()=>{}}
+                onClick={handleSubmit}
                 className='text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer'
               >
                 Send now
